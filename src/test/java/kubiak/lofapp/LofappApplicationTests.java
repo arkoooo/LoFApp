fa4096eaf3278a2c8ff75694db04ffd1b41ce984
Arko <spontanicznie94@gmail.com>
2020-06-27 11:46:03 +0200

    first

---
+++ src/test/java/kubiak/lofapp/LofappApplicationTests.java
@@ -0,0 +1,13 @@
+package kubiak.lofapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
